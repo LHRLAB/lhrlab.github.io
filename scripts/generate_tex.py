77430c891f1e2f6f4a684a5dca6728b389ceacf5
LHRLAB <LHRLAB@users.noreply.github.com>
2025-08-01 06:09:22 +0000

    deploy: ff5177f2d712a3fe357dd11ac57a031b90db99e9

---
+++ scripts/generate_tex.py
@@ -0,0 +1,20 @@
+import yaml
+import os
+
+with open("resume/template/resume_template.tex", "r") as f:
+    resume_template = f.read()
+
+
+with open("_pages/projects.yml", "r") as f:
+    data = yaml.safe_load(f)
+
+s = ""
+s += "\\section{Papers}\n\\resumeSubHeadingListStart\n"
+for p in data["projects"]:
+    s += f"  \\resumeSubItem{{{p['title']}}}{{{p['description']}}}\n"
+s += "\\resumeSubHeadingListEnd\n"
+
+resume_template = resume_template.replace("##PROJECTS##", s)
+
+with open("resume/resume.tex", "w") as f:
+    f.write(resume_template)
