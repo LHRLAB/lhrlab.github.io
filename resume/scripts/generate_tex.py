104c090d274297b855966c6aa3f3c459409fd82e
luohaoran <luohaoran@bupt.edu.cn>
2025-08-02 17:07:49 +0800

    add

--- resume/scripts/generate_tex.py
+++ resume/scripts/generate_tex.py
@@ -4,17 +4,16 @@ import os
 with open("resume/template/resume_template.tex", "r") as f:
     resume_template = f.read()
 
+# with open("_pages/projects.yml", "r") as f:
+#     data = yaml.safe_load(f)
 
-with open("_pages/projects.yml", "r") as f:
-    data = yaml.safe_load(f)
-
-s = ""
-s += "\\section{Papers}\n\\resumeSubHeadingListStart\n"
-for p in data["projects"]:
-    s += f"  \\resumeSubItem{{{p['title']}}}{{{p['description']}}}\n"
-s += "\\resumeSubHeadingListEnd\n"
+# s = ""
+# s += "\\section{Papers}\n\\resumeSubHeadingListStart\n"
+# for p in data["projects"]:
+#     s += f"  \\resumeSubItem{{{p['title']}}}{{{p['description']}}}\n"
+# s += "\\resumeSubHeadingListEnd\n"
 
 # resume_template = resume_template.replace("##PROJECTS##", s)
 
-with open("resume/resume.tex", "w") as f:
+with open("resume/HaoranLuo_CV.tex", "w") as f:
     f.write(resume_template)
