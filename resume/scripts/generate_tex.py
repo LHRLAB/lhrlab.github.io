952acda167e60fbb94e76af97f0e41369d0e3120
luohaoran <luohaoran@bupt.edu.cn>
2025-08-02 15:43:57 +0800

    add

--- resume/scripts/generate_tex.py
+++ resume/scripts/generate_tex.py
@@ -14,7 +14,7 @@ for p in data["projects"]:
     s += f"  \\resumeSubItem{{{p['title']}}}{{{p['description']}}}\n"
 s += "\\resumeSubHeadingListEnd\n"
 
-resume_template = resume_template.replace("##PROJECTS##", s)
+# resume_template = resume_template.replace("##PROJECTS##", s)
 
 with open("resume/resume.tex", "w") as f:
     f.write(resume_template)
